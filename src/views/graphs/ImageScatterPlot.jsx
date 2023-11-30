6be06863a366d72052b53e970b8f21fec30cff0f
daviprios <davipaimrios@gmail.com>
2023-11-30 15:27:16 -0300

    add image scatter plot styling

--- src/views/graphs/ImageScatterPlot.jsx
+++ src/views/graphs/ImageScatterPlot.jsx
@@ -1,7 +1,8 @@
 import * as Plot from "@observablehq/plot";
+import * as d3 from "d3";
 import { useEffect, useRef, useState } from "react";
 
-import json from "./testData";
+import json from "../../data/testData";
 import { useMatchContext } from "../../contexts/matchContext";
 
 export default function ImageScatterPlot() {
@@ -9,6 +10,7 @@ export default function ImageScatterPlot() {
   const containerRef = useRef(null);
 
   const [playerData] = useState(() => {
+    let totalPicks = 0;
     const formatedData = json
       .map(({ info: { participants } }) =>
         participants.find(({ puuid }) => puuid === playerUuid)
@@ -20,6 +22,7 @@ export default function ImageScatterPlot() {
       }))
       .reduce((acc, { championName, win }) => {
         const stored = acc.get(championName);
+        ++totalPicks;
         return acc.set(championName, {
           wins: win ? (stored?.wins ?? 0) + 1 : stored?.wins ?? 0,
           picks: (stored?.picks ?? 0) + 1,
@@ -31,7 +34,7 @@ export default function ImageScatterPlot() {
       data.push({
         championName: k,
         winRate: v.wins / v.picks,
-        pickRate: v.picks,
+        pickRate: v.picks / totalPicks,
       });
     });
     return data;
@@ -42,16 +45,33 @@ export default function ImageScatterPlot() {
 
     const plot = Plot.plot({
       inset: 20,
-      x: { label: "Taxa de escolha →" },
-      y: { label: "↑ Taxa de vitória", grid: true },
+      x: { label: "Taxa de escolha →", grid: true, tickFormat: ".0%" },
+      y: { label: "↑ Taxa de vitória", grid: true, tickFormat: ".0%" },
       marks: [
         Plot.ruleY([0]),
+        Plot.ruleX([0]),
+        Plot.areaY([[0], [d3.max(playerData, (d) => d.pickRate) ?? 0.1]], {
+          fillOpacity: 0.1,
+          fill: "red",
+          x: "0",
+          y1: 0,
+          y2: 0.5,
+        }),
+        Plot.areaY([[0], [d3.max(playerData, (d) => d.pickRate) ?? 0.1]], {
+          fillOpacity: 0.1,
+          fill: "green",
+          x: "0",
+          y1: 0.5,
+          y2: 1,
+        }),
         Plot.image(playerData, {
           x: "pickRate",
           y: "winRate",
-          src: () => "https://via.placeholder.com/100x100",
+          r: 20,
+          preserveAspectRatio: "xMidYMin slice",
+          src: (d) => `/champions/${d.championName}.png`,
           width: 40,
-          title: () => "championName",
+          title: "championName",
         }),
       ],
     });
