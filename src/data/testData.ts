3da98e412ef93c5a0c6fd879638b4729ec25253d
daviprios <davipaimrios@gmail.com>
2023-12-04 14:58:17 -0300

    improve build

--- src/data/testData.ts
+++ src/data/testData.ts
@@ -18,86 +18,86 @@ import json17 from "../assets/data/BR1_2737960805-info.json";
 import json18 from "../assets/data/BR1_2737997009-info.json";
 import json19 from "../assets/data/BR1_2738117091-info.json";
 import json20 from "../assets/data/BR1_2738129965-info.json";
-import json21 from "../assets/data/BR1_2738388306-info.json";
-import json22 from "../assets/data/BR1_2738391734-info.json";
-import json23 from "../assets/data/BR1_2738400120-info.json";
-import json24 from "../assets/data/BR1_2738497441-info.json";
-import json25 from "../assets/data/BR1_2738514007-info.json";
-import json26 from "../assets/data/BR1_2738553594-info.json";
-import json27 from "../assets/data/BR1_2738569432-info.json";
-import json28 from "../assets/data/BR1_2738580624-info.json";
-import json29 from "../assets/data/BR1_2738685867-info.json";
-import json30 from "../assets/data/BR1_2738719213-info.json";
-import json31 from "../assets/data/BR1_2738739546-info.json";
-import json32 from "../assets/data/BR1_2738764946-info.json";
-import json33 from "../assets/data/BR1_2739368931-info.json";
-import json34 from "../assets/data/BR1_2739382748-info.json";
-import json35 from "../assets/data/BR1_2740240179-info.json";
-import json36 from "../assets/data/BR1_2740261363-info.json";
-import json37 from "../assets/data/BR1_2740618114-info.json";
-import json38 from "../assets/data/BR1_2740640633-info.json";
-import json39 from "../assets/data/BR1_2740725496-info.json";
-import json40 from "../assets/data/BR1_2740742742-info.json";
-import json41 from "../assets/data/BR1_2741277431-info.json";
-import json42 from "../assets/data/BR1_2741322777-info.json";
-import json43 from "../assets/data/BR1_2741366031-info.json";
-import json44 from "../assets/data/BR1_2741397111-info.json";
-import json45 from "../assets/data/BR1_2741855832-info.json";
-import json46 from "../assets/data/BR1_2741889294-info.json";
-import json47 from "../assets/data/BR1_2741917158-info.json";
-import json48 from "../assets/data/BR1_2741947403-info.json";
-import json49 from "../assets/data/BR1_2742425712-info.json";
-import json50 from "../assets/data/BR1_2742446229-info.json";
-import json51 from "../assets/data/BR1_2742458781-info.json";
-import json52 from "../assets/data/BR1_2742482066-info.json";
-import json53 from "../assets/data/BR1_2742498323-info.json";
-import json54 from "../assets/data/BR1_2742634417-info.json";
-import json55 from "../assets/data/BR1_2743144796-info.json";
-import json56 from "../assets/data/BR1_2743664397-info.json";
-import json57 from "../assets/data/BR1_2743707390-info.json";
-import json58 from "../assets/data/BR1_2743728808-info.json";
-import json59 from "../assets/data/BR1_2744080428-info.json";
-import json60 from "../assets/data/BR1_2744107099-info.json";
-import json61 from "../assets/data/BR1_2744692952-info.json";
-import json62 from "../assets/data/BR1_2745121615-info.json";
-import json63 from "../assets/data/BR1_2745136310-info.json";
-import json64 from "../assets/data/BR1_2745159228-info.json";
-import json65 from "../assets/data/BR1_2746606278-info.json";
-import json66 from "../assets/data/BR1_2746617229-info.json";
-import json67 from "../assets/data/BR1_2746631833-info.json";
-import json68 from "../assets/data/BR1_2746651750-info.json";
-import json69 from "../assets/data/BR1_2746683928-info.json";
-import json70 from "../assets/data/BR1_2746705073-info.json";
-import json71 from "../assets/data/BR1_2746840392-info.json";
-import json72 from "../assets/data/BR1_2746855655-info.json";
-import json73 from "../assets/data/BR1_2746871542-info.json";
-import json74 from "../assets/data/BR1_2747660225-info.json";
-import json75 from "../assets/data/BR1_2747681601-info.json";
-import json76 from "../assets/data/BR1_2747715366-info.json";
-import json77 from "../assets/data/BR1_2747734510-info.json";
-import json78 from "../assets/data/BR1_2748126446-info.json";
-import json79 from "../assets/data/BR1_2748158078-info.json";
-import json80 from "../assets/data/BR1_2749428672-info.json";
-import json81 from "../assets/data/BR1_2749723718-info.json";
-import json82 from "../assets/data/BR1_2749758103-info.json";
-import json83 from "../assets/data/BR1_2749768766-info.json";
-import json84 from "../assets/data/BR1_2750609149-info.json";
-import json85 from "../assets/data/BR1_2750811055-info.json";
-import json86 from "../assets/data/BR1_2750826113-info.json";
-import json87 from "../assets/data/BR1_2751229334-info.json";
-import json88 from "../assets/data/BR1_2751672873-info.json";
-import json89 from "../assets/data/BR1_2751697078-info.json";
-import json90 from "../assets/data/BR1_2751821807-info.json";
-import json91 from "../assets/data/BR1_2751845594-info.json";
-import json92 from "../assets/data/BR1_2752324792-info.json";
-import json93 from "../assets/data/BR1_2752829257-info.json";
-import json94 from "../assets/data/BR1_2753322678-info.json";
-import json95 from "../assets/data/BR1_2753345635-info.json";
-import json96 from "../assets/data/BR1_2753368195-info.json";
-import json97 from "../assets/data/BR1_2754062526-info.json";
-import json98 from "../assets/data/BR1_2754086648-info.json";
-import json99 from "../assets/data/BR1_2754101546-info.json";
-import json100 from "../assets/data/BR1_2754165472-info.json";
+// import json21 from "../assets/data/BR1_2738388306-info.json";
+// import json22 from "../assets/data/BR1_2738391734-info.json";
+// import json23 from "../assets/data/BR1_2738400120-info.json";
+// import json24 from "../assets/data/BR1_2738497441-info.json";
+// import json25 from "../assets/data/BR1_2738514007-info.json";
+// import json26 from "../assets/data/BR1_2738553594-info.json";
+// import json27 from "../assets/data/BR1_2738569432-info.json";
+// import json28 from "../assets/data/BR1_2738580624-info.json";
+// import json29 from "../assets/data/BR1_2738685867-info.json";
+// import json30 from "../assets/data/BR1_2738719213-info.json";
+// import json31 from "../assets/data/BR1_2738739546-info.json";
+// import json32 from "../assets/data/BR1_2738764946-info.json";
+// import json33 from "../assets/data/BR1_2739368931-info.json";
+// import json34 from "../assets/data/BR1_2739382748-info.json";
+// import json35 from "../assets/data/BR1_2740240179-info.json";
+// import json36 from "../assets/data/BR1_2740261363-info.json";
+// import json37 from "../assets/data/BR1_2740618114-info.json";
+// import json38 from "../assets/data/BR1_2740640633-info.json";
+// import json39 from "../assets/data/BR1_2740725496-info.json";
+// import json40 from "../assets/data/BR1_2740742742-info.json";
+// import json41 from "../assets/data/BR1_2741277431-info.json";
+// import json42 from "../assets/data/BR1_2741322777-info.json";
+// import json43 from "../assets/data/BR1_2741366031-info.json";
+// import json44 from "../assets/data/BR1_2741397111-info.json";
+// import json45 from "../assets/data/BR1_2741855832-info.json";
+// import json46 from "../assets/data/BR1_2741889294-info.json";
+// import json47 from "../assets/data/BR1_2741917158-info.json";
+// import json48 from "../assets/data/BR1_2741947403-info.json";
+// import json49 from "../assets/data/BR1_2742425712-info.json";
+// import json50 from "../assets/data/BR1_2742446229-info.json";
+// import json51 from "../assets/data/BR1_2742458781-info.json";
+// import json52 from "../assets/data/BR1_2742482066-info.json";
+// import json53 from "../assets/data/BR1_2742498323-info.json";
+// import json54 from "../assets/data/BR1_2742634417-info.json";
+// import json55 from "../assets/data/BR1_2743144796-info.json";
+// import json56 from "../assets/data/BR1_2743664397-info.json";
+// import json57 from "../assets/data/BR1_2743707390-info.json";
+// import json58 from "../assets/data/BR1_2743728808-info.json";
+// import json59 from "../assets/data/BR1_2744080428-info.json";
+// import json60 from "../assets/data/BR1_2744107099-info.json";
+// import json61 from "../assets/data/BR1_2744692952-info.json";
+// import json62 from "../assets/data/BR1_2745121615-info.json";
+// import json63 from "../assets/data/BR1_2745136310-info.json";
+// import json64 from "../assets/data/BR1_2745159228-info.json";
+// import json65 from "../assets/data/BR1_2746606278-info.json";
+// import json66 from "../assets/data/BR1_2746617229-info.json";
+// import json67 from "../assets/data/BR1_2746631833-info.json";
+// import json68 from "../assets/data/BR1_2746651750-info.json";
+// import json69 from "../assets/data/BR1_2746683928-info.json";
+// import json70 from "../assets/data/BR1_2746705073-info.json";
+// import json71 from "../assets/data/BR1_2746840392-info.json";
+// import json72 from "../assets/data/BR1_2746855655-info.json";
+// import json73 from "../assets/data/BR1_2746871542-info.json";
+// import json74 from "../assets/data/BR1_2747660225-info.json";
+// import json75 from "../assets/data/BR1_2747681601-info.json";
+// import json76 from "../assets/data/BR1_2747715366-info.json";
+// import json77 from "../assets/data/BR1_2747734510-info.json";
+// import json78 from "../assets/data/BR1_2748126446-info.json";
+// import json79 from "../assets/data/BR1_2748158078-info.json";
+// import json80 from "../assets/data/BR1_2749428672-info.json";
+// import json81 from "../assets/data/BR1_2749723718-info.json";
+// import json82 from "../assets/data/BR1_2749758103-info.json";
+// import json83 from "../assets/data/BR1_2749768766-info.json";
+// import json84 from "../assets/data/BR1_2750609149-info.json";
+// import json85 from "../assets/data/BR1_2750811055-info.json";
+// import json86 from "../assets/data/BR1_2750826113-info.json";
+// import json87 from "../assets/data/BR1_2751229334-info.json";
+// import json88 from "../assets/data/BR1_2751672873-info.json";
+// import json89 from "../assets/data/BR1_2751697078-info.json";
+// import json90 from "../assets/data/BR1_2751821807-info.json";
+// import json91 from "../assets/data/BR1_2751845594-info.json";
+// import json92 from "../assets/data/BR1_2752324792-info.json";
+// import json93 from "../assets/data/BR1_2752829257-info.json";
+// import json94 from "../assets/data/BR1_2753322678-info.json";
+// import json95 from "../assets/data/BR1_2753345635-info.json";
+// import json96 from "../assets/data/BR1_2753368195-info.json";
+// import json97 from "../assets/data/BR1_2754062526-info.json";
+// import json98 from "../assets/data/BR1_2754086648-info.json";
+// import json99 from "../assets/data/BR1_2754101546-info.json";
+// import json100 from "../assets/data/BR1_2754165472-info.json";
 
 const testData = [
   json1,
@@ -120,85 +120,85 @@ const testData = [
   json18,
   json19,
   json20,
-  json21,
-  json22,
-  json23,
-  json24,
-  json25,
-  json26,
-  json27,
-  json28,
-  json29,
-  json30,
-  json31,
-  json32,
-  json33,
-  json34,
-  json35,
-  json36,
-  json37,
-  json38,
-  json39,
-  json40,
-  json41,
-  json42,
-  json43,
-  json44,
-  json45,
-  json46,
-  json47,
-  json48,
-  json49,
-  json50,
-  json51,
-  json52,
-  json53,
-  json54,
-  json55,
-  json56,
-  json57,
-  json58,
-  json59,
-  json60,
-  json61,
-  json62,
-  json63,
-  json64,
-  json65,
-  json66,
-  json67,
-  json68,
-  json69,
-  json70,
-  json71,
-  json72,
-  json73,
-  json74,
-  json75,
-  json76,
-  json77,
-  json78,
-  json79,
-  json80,
-  json81,
-  json82,
-  json83,
-  json84,
-  json85,
-  json86,
-  json87,
-  json88,
-  json89,
-  json90,
-  json91,
-  json92,
-  json93,
-  json94,
-  json95,
-  json96,
-  json97,
-  json98,
-  json99,
-  json100,
+  // json21,
+  // json22,
+  // json23,
+  // json24,
+  // json25,
+  // json26,
+  // json27,
+  // json28,
+  // json29,
+  // json30,
+  // json31,
+  // json32,
+  // json33,
+  // json34,
+  // json35,
+  // json36,
+  // json37,
+  // json38,
+  // json39,
+  // json40,
+  // json41,
+  // json42,
+  // json43,
+  // json44,
+  // json45,
+  // json46,
+  // json47,
+  // json48,
+  // json49,
+  // json50,
+  // json51,
+  // json52,
+  // json53,
+  // json54,
+  // json55,
+  // json56,
+  // json57,
+  // json58,
+  // json59,
+  // json60,
+  // json61,
+  // json62,
+  // json63,
+  // json64,
+  // json65,
+  // json66,
+  // json67,
+  // json68,
+  // json69,
+  // json70,
+  // json71,
+  // json72,
+  // json73,
+  // json74,
+  // json75,
+  // json76,
+  // json77,
+  // json78,
+  // json79,
+  // json80,
+  // json81,
+  // json82,
+  // json83,
+  // json84,
+  // json85,
+  // json86,
+  // json87,
+  // json88,
+  // json89,
+  // json90,
+  // json91,
+  // json92,
+  // json93,
+  // json94,
+  // json95,
+  // json96,
+  // json97,
+  // json98,
+  // json99,
+  // json100,
 ];
 export default testData;
